14342a1f427ff7cfd15abd7a0b55b164b0b5a75c
Prudhvi Maguluru <prudhvimaguluru@Prudhvis-MacBook-Pro.local>
2026-09-08 09:02:34 +0530

    Add evaluations_system_test.rb to satisfy bin/rails test:system in GitHub Actions

---
+++ test/system/evaluations_system_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class EvaluationsSystemTest < ApplicationSystemTestCase
+  test "visiting the evaluation arena homepage" do
+    visit root_url
+    assert_selector "h1", text: "AI Model Evaluation Arena"
+  end
+end
