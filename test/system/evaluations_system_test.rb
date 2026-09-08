92bc6b48e811b8dbd4a90146b789f513caed0bc3
Prudhvi Maguluru <prudhvimaguluru@Prudhvis-MacBook-Pro.local>
2026-09-08 09:07:54 +0530

    Use require_relative for application_system_test_case in system test

--- test/system/evaluations_system_test.rb
+++ test/system/evaluations_system_test.rb
@@ -1,4 +1,4 @@
-require "application_system_test_case"
+require_relative "application_system_test_case"
 
 class EvaluationsSystemTest < ApplicationSystemTestCase
   test "visiting the evaluation arena homepage" do
